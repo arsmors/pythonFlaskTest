700dced8d93db70a4fba21eaa7d4f5243750e5cb
arsens <arsens.morins@gmail.com>
2019-07-01 11:31:54 +0000

    b2

--- flask_app.py
+++ flask_app.py
@@ -57,6 +57,7 @@ class Comment(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
+    posted = db.Column(db.DateTime, default=datetime.now)
 
 
 
@@ -64,7 +65,7 @@ class Comment(db.Model):
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("main_page.html", comments=Comment.query.all(), timestamp=datetime.now())
+        return render_template("main_page.html", comments=Comment.query.all())
 
     comment = Comment(content=request.form["contents"])
     if not current_user.is_authenticated:
